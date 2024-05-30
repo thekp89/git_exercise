f2ea9af85f2cd7cb9ae407a58fd61aa3da9170f7
thekp89 <luis.palaciosgutz@gmail.com>
2024-05-29 19:12:22 -0600

    Initial project structure

---
+++ src/js/main.js/main.js
@@ -0,0 +1,3 @@
+document.getElementById('check').addEventListener('click',function(){
+	const password = document.getElementById('password').value;
+	document.getElementById('feedback').textContent =`Password length: ${password.length}`;});
